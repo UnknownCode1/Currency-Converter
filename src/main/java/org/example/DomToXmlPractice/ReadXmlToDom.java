4b139d166a7d5e7ac66d346e245c148a3ab5eb53
UnknownCode1 <balamshivaprasad2001@gmail.com>
2025-03-01 13:21:19 +0530

    Added ReadXmlToDom class

---
+++ src/main/java/org/example/DomToXmlPractice/ReadXmlToDom.java
@@ -0,0 +1,39 @@
+package org.example.DomToXmlPractice;
+
+import org.w3c.dom.Document;
+import org.w3c.dom.Node;
+import org.w3c.dom.NodeList;
+import org.xml.sax.SAXException;
+
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.parsers.ParserConfigurationException;
+import java.io.File;
+import java.io.IOException;
+
+public class ReadXmlToDom {
+    public void readXmlData(String path, String tagName){
+        File xmlfile = new File(path);
+
+        DocumentBuilderFactory factory = DocumentBuilderFactory.newInstance();
+        DocumentBuilder builder;
+        try{
+            builder= factory.newDocumentBuilder();
+        } catch (ParserConfigurationException e) {
+            throw new RuntimeException(e);
+        }
+
+        Document document;
+        try{
+            document = builder.parse(xmlfile);
+        } catch (IOException | SAXException e) {
+            throw new RuntimeException(e);
+        }
+
+        NodeList nodes = document.getElementsByTagName(tagName);
+        for(int i=0; i<nodes.getLength(); i++){
+            Node node = nodes.item(i);
+            System.out.println(node.getTextContent()+" ");
+        }
+    }
+}
--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,11 +1,9 @@
 package org.example;
 
 
-import org.example.DomToXmlPractice.DomToXmlPractice;
 
 class Main{
     public static void main(String []args) {
-        DomToXmlPractice obj = new DomToXmlPractice();
-        System.out.println(obj.writeToXml("C:\\Users\\shiva\\IdeaProjects\\practice-Projects\\CurrencyConnverter\\output.xml"));
+
     }
 }
